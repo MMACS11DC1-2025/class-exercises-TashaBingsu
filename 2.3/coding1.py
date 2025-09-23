6e6812f5a1b1c762441ed95d75f3893fed8e1642
TashaBingsu <1532353@learn.vsb.bc.ca>
2025-09-23 21:41:12 +0000

    yello

--- 2.3/coding1.py
+++ 2.3/coding1.py
@@ -11,3 +11,11 @@ How old are you?
 > 25
 In 2056, you will be 56 years old!
 """
+
+age = int(input("How old are you? "))
+result = age + 31
+
+if age == 4:
+    print("YOU ARE THE YOUNGEST PERSON EVER!!!!")
+else:
+    print("You will be " + str(result) + " years old in 2056!")
