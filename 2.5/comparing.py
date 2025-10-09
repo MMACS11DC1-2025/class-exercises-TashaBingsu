f827cfbc588787c361f3780aa01d2a394c3672f3
TashaBingsu <1532353@learn.vsb.bc.ca>
2025-10-09 09:49:31 +0000

    hi

--- 2.5/comparing.py
+++ 2.5/comparing.py
@@ -10,18 +10,69 @@ Test as you go! Describe in your comments what steps you took to test your code.
 file = open("2.4/responses.csv")
 junk = file.readline()
 
-print("Hello. Please enter your full name! We will be comparing you with another person from this class to see how compatible you two are.")
+print("Hello. Please enter your name! We will be comparing you with another person from this class to see how compatible you two are.")
 yourname = input("Your name is: ").lower().strip()
 
-print("Ok awesomesauce -_-. Now enter your classmates name please.")
-name2 = input("Classmate's name: ").lower().strip()
-
-reccomendation= []
+# initialize variables
 yourname_list = []
 name2_list = []
+found_yourname = False
+found_name2 = False
 
-for hi in file:
-if str(yourname) in hi.lower():
-yourname_list = hi.split(",")
+# first look for the first name
+for line in file:
+    line_lower = line.lower()
+    if yourname in line_lower and not found_yourname:
+        yourname_list = line.strip().split(",")
+        found_yourname = True
+        break  # breaking because first name was found
 
+# only ask for second name if first name was found
+if found_yourname:
+    print("Ok awesomesauce -_-. Now enter your classmates name please.")
+    name2 = input("Classmate's name: ").lower().strip()
+    
+    # reset the file to look for second name
+    file.seek(0)  # going back to start of file(i found this on youtube)
+    junk = file.readline()  # skipping header again
+    
+    # now looking for second name
+    for line in file:
+        line_lower = line.lower()
+        if name2 in line_lower and not found_name2:
+            name2_list = line.strip().split(",")
+            found_name2 = True
+            break
+else:
+    quit("I don't have '" + yourname + "' in my list yo... Try again")
 
+# checking if second name was found
+if not found_name2:
+    quit("I don't have '" + name2 + "' in my list yo... Try again")
+
+# compare the responses (skip the first column which is the name)
+similar = 0
+total_qs = min(len(yourname_list), len(name2_list)) - 1  # minus 1 to exclude name
+
+for i in range(1, total_qs + 1):  # start from 1 to skip name column
+    if i < len(yourname_list) and i < len(name2_list):  # safety check!!!!!
+        if yourname_list[i].strip().lower() == name2_list[i].strip().lower():
+            similar += 1
+
+if similar == 0:
+    print("Ok.. so you guys have 0 things in common. Hopefully you can still be friends!")
+elif similar == 1:
+    print("You have 1 thing in common with eachother! Maybe you guys like the same TV show then...?")
+elif similar <= 3:
+    print("Cool cool, you guys have " + str(similar) + " things in common. Be their friend :p")
+elif similar <= 5:
+    print("Nice! You guys have " + str(similar) + " things in common! That's pretty similar.")
+elif similar <= 7:
+    print("Alrighttt, I see good friends in the making. You guys have " + str(similar) + " things in common!")
+elif similar == 8:
+    print("P-P-Pause!! You guys have everything in common! Is this your twin perhaps?")
+else:
+    print("Hm.")
+
+compatibility = (similar / total_qs) * 100
+print("Your compatibility percent is: " + compatibility)
