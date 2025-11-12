1fbd65d251c6fb2c4b8bb0e0d1b2ad460b0144e5
TashaBingsu <1532353@learn.vsb.bc.ca>
2025-11-12 21:35:10 +0000

    one more game

--- 5.1/image_open.py
+++ 5.1/image_open.py
@@ -1,4 +1,19 @@
 from PIL import Image
 
-image_green = Image.open("CS11/5.1/kid-green.jpg").load()
-image_beach = Image.open("CS11/5.1/beach.jpg").load()
+image_green = Image.open("5.1/kid-green.jpg").load()
+image_beach = Image.open("5.1/beach.jpg").load()
+
+def is_green(r, g, b):
+    if r >= 0 r < 25 and g > 230 and g <= 255 and b >= 0 and b < 25:
+        return True
+    else:
+        return False
+
+image_green = Image.open("kid-green.jpg").load()
+image_beach = Image.open("beach.jpg").load()
+
+print(image_greem[0,0])
+
+pixel = image_green[0,0]
+r = pixel[0]
+g = image_green
