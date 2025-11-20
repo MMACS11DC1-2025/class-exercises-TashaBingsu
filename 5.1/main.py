a504631aaeb2bfa2dc12c0f5a381c51417a02f0a
TashaBingsu <1532353@learn.vsb.bc.ca>
2025-11-20 21:15:14 +0000

    bread taste better than key

--- 5.1/main.py
+++ 5.1/main.py
@@ -3,16 +3,19 @@ from PIL import Image
 def colour(r, g, b):
     if r > 150 and g > 150 and b < 150:
         return "yellow"
+    elif r < 30 and g < 30 and b < 30:
+        return "black"
     else:
         return "other"
     
-file = Image.open("jelly_beans.jpg")
+file = Image.open("5.1/jelly_beans.jpg")
 jb_image = file.load()
 
 print(jb_image[0,0])
 print(colour(jb_image[0,0][0], jb_image[0,0][1], jb_image[0,0][2]))
 
 yellow_pixels = []
+black_pixels = []
 
 width = file.width
 height = file.height
@@ -25,6 +28,17 @@ for x in range(width):
 
         if colour(pixel_r, pixel_g, pixel_b) == "yellow":
             yellow_pixels.append(jb_image[x,y])
+            file.putpixel((x,y), (255, 255, 255))
+
+        if colour(pixel_r, pixel_g, pixel_b) == "black":
+            black_pixels.append(jb_image[x,y])
+            file.putpixel((x,y), (255, 255, 255))
+
+num_yellow = len(yellow_pixels)
+total_pixels = width*height
+file.save("output.png", "png")
+
+yellow_ratio = num_yellow / total_pixels
 
 print(len(yellow_pixels))
 print(width*height)
