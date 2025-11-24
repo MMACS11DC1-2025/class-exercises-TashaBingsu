ef0391eebf28e26a41cbedb84dd13ce240528c2f
TashaBingsu <1532353@learn.vsb.bc.ca>
2025-11-24 21:35:11 +0000

    guys

--- 5.1/main.py
+++ 5.1/main.py
@@ -1,24 +1,42 @@
+import time
+
+t0 = time.time()
+
 from PIL import Image
+
+t1 = time.time()
+
 
 def colour(r, g, b):
     if r > 150 and g > 150 and b < 150:
         return "yellow"
-    elif r < 30 and g < 30 and b < 30:
+    elif r > 160 and g > 0 and b > 1:
+        return "red"
+    elif r > 15 and g > 90 and b > 15:
+        return "green"
+    elif r > 45 and g > 55 and b > 178:
+        return "blue"
+    elif r > 255 and g > 255 and b > 255:
         return "black"
+    elif r > 210 and g > 110 and b < 10:
+        return "orange"
     else:
         return "other"
-    
+
+   
 file = Image.open("5.1/jelly_beans.jpg")
 jb_image = file.load()
 
-print(jb_image[0,0])
-print(colour(jb_image[0,0][0], jb_image[0,0][1], jb_image[0,0][2]))
-
 yellow_pixels = []
-black_pixels = []
+red_pixels = []
+green_pixels = []
+blue_pixels= []
+orange_pixels = []
 
 width = file.width
 height = file.height
+
+t2 = time.time()
 
 for x in range(width):
     for y in range(height):
@@ -28,17 +46,61 @@ for x in range(width):
 
         if colour(pixel_r, pixel_g, pixel_b) == "yellow":
             yellow_pixels.append(jb_image[x,y])
-            file.putpixel((x,y), (255, 255, 255))
-
+            file.putpixel((x, y), (255, 255, 255))
+        if colour(pixel_r, pixel_g, pixel_b) == "red":
+            red_pixels.append(jb_image[x,y])
+        if colour(pixel_r, pixel_g, pixel_b) == "green":
+            green_pixels.append(jb_image[x,y])
+        if colour(pixel_r, pixel_g, pixel_b) == "blue":
+            blue_pixels.append(jb_image[x,y])
         if colour(pixel_r, pixel_g, pixel_b) == "black":
             black_pixels.append(jb_image[x,y])
-            file.putpixel((x,y), (255, 255, 255))
+        if colour(pixel_r, pixel_g, pixel_b) == "orange":
+           orange_pixels.append(jb_image[x,y])
 
+
+t3 = time.time()
+       
 num_yellow = len(yellow_pixels)
+num_red = len(red_pixels)
+num_green = len(green_pixels)
+num_blue = len(blue_pixels)
+num_black = len(blue_pixels)
+num_orange = len(orange_pixels)
+
+
 total_pixels = width*height
 file.save("output.png", "png")
 
-yellow_ratio = num_yellow / total_pixels
 
-print(len(yellow_pixels))
-print(width*height)
+yellow_ratio = num_yellow / total_pixels
+red_ratio = num_red / total_pixels
+green_ratio = num_green / total_pixels
+blue_ratio = num_blue / total_pixels
+orange_ratio = num_orange / total_pixels
+
+
+num_yellow = len(yellow_pixels)
+num_red = len(red_pixels)
+num_green = len(green_pixels)
+num_blue = len(blue_pixels)
+num_orange = len(orange_pixels)
+
+yellow_percent = yellow_ratio * 100
+red_percent = red_ratio * 100
+green_percent = green_ratio * 100
+blue_percent = blue_ratio * 100
+orange_percent = orange_ratio * 100
+
+report = "there are {:.2f}% yellow jelly beans, {:.2f}% red jelly beans, {:.2f}% green jelly beans," \
+          "{:.2f}% blue jelly beans, {:.2f}% orange jelly beans".format(yellow_percent, red_percent, green_percent, blue_percent, orange_percent)
+print(report)
+
+module_load = t1- t0
+image_open_load = t2 - t1
+loop = t3 - t2
+entire = t3 - t0
+
+timings = "it took {:.2f}s to import PIL, {:.2f}s to load the image," \
+            " {:.2f}s to do the loop. All in all it took {:.2f}s".format(module_load, image_open_load, loop, entire)
+print(timings)
