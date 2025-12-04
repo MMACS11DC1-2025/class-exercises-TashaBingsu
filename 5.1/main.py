eee7ee51978d50e8a168bbb1542621cce92b9055
TashaBingsu <1532353@learn.vsb.bc.ca>
2025-12-04 21:14:47 +0000

    its the end of class

--- 5.1/main.py
+++ 5.1/main.py
@@ -1,9 +1,7 @@
 import time
 
 t0 = time.time()
-
 from PIL import Image
-
 t1 = time.time()
 
 
--- 6.7/main.py
+++ 6.7/main.py
@@ -4,4 +4,17 @@ t0 = time.time()
 from PIL import Image
 t1 = time.time()
 
+image1 = Image.open("6.7/lung1.png").load()
+image2 = Image.open("6.7/lung2.png").load()
+image3 = Image.open("6.7/lung3.png").load()
+image4 = Image.open("6.7/lung4.png").load()
+image5 = Image.open("6.7/lung5.png").load()
+image6 = Image.open("6.7/lung6.png").load()
+image7 = Image.open("6.7/lung7.png").load()
+image8 = Image.open("6.7/lung8.png").load()
+image9 = Image.open("6.7/lung9.png").load()
+image10 = Image.open("6.7/lung10.png").load()
 
+def colour(r, g, b):
+    if 150 <= r <= 180 and 150 <= b <= 180 and 150 <= g <= 180
+        return "pneumonia alert!" 
