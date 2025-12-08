2e9895924406683a89e644a590b44dcb6525f4c6
TashaBingsu <1532353@learn.vsb.bc.ca>
2025-12-08 21:34:40 +0000

    happier by marshemllow

--- 6.7/main.py
+++ 6.7/main.py
@@ -13,6 +13,25 @@ for i in images:
 def colour(r, g, b):
     if 150 <= r <= 180 and 150 <= b <= 180 and 150 <= g <= 180
         return "grey"
-    
-width = file.width
-height = file.height
+
+t2 = time.time()
+
+for i in range(len(images)):
+    width = file.width
+    height = file.height
+
+    grey_pixels = []
+    white_pixels = []
+    black_pixels = []
+
+    for x in range(width):
+        for y in range(height):
+            pixel_r = lungimage[x,y][0]
+            pixel_g = lungimage[x,y][1]
+            pixel_b = lungimage[x,y][2]
+
+            if colour(pixel_r, pixel_g, pixel_b) == "grey":
+                grey_pixels.append(lungimage[x,y])
+                file.putpixel((x, y), (255, 255, 255))
+            if colour(pixel_r, pixel_g, pixel_b) == "white":
+                white_pixels.append(lungimage[x,y])
