be76e8cab04ec9c5ca3ddf31031b0af0797ea02a
TashaBingsu <1532353@learn.vsb.bc.ca>
2025-12-09 21:14:52 +0000

    chow mein

--- 6.7/main.py
+++ 6.7/main.py
@@ -5,14 +5,26 @@ from PIL import Image
 t1 = time.time()
 
 images = ["6.7/lung1.png", "6.7/lung1.png", "6.7/lung1.png", "6.7/lung1.png", "6.7/lung1.png", "6.7/lung1.png", "6.7/lung1.png", "6.7/lung1.png", "6.7/lung1.png", "6.7/lung1.png" ]
+ 
+newr = int((r+g+b)/3)
+newg = int((r+g+b)/3)
+newb = int((r+g+b)/3)
+
+newpixel = (newr, newg, newb)
+
+lungs_grey.putpixel((col,row), newpixel)
 
 for i in images:
     file = Image.open(i)
     lungimage = file.load()
 
 def colour(r, g, b):
-    if 150 <= r <= 180 and 150 <= b <= 180 and 150 <= g <= 180
+    if 150 <= r <= 180 and 150 <= b <= 180 and 150 <= g <= 180:
         return "grey"
+    elif r > 240 and g > 240 and b > 240:
+        return "white"
+    elif r > 0 and g > 0 and b > 0:
+        return "black"
 
 t2 = time.time()
 
@@ -32,6 +44,13 @@ for i in range(len(images)):
 
             if colour(pixel_r, pixel_g, pixel_b) == "grey":
                 grey_pixels.append(lungimage[x,y])
-                file.putpixel((x, y), (255, 255, 255))
             if colour(pixel_r, pixel_g, pixel_b) == "white":
                 white_pixels.append(lungimage[x,y])
+            if colour(pixel_r, pixel_g, pixel_b) == "black":
+                white_pixels.append(lungimage[x,y])
+
+num_grey = len(grey_pixels)
+num_white = len(white_pixels)
+num_black = len(black_pixels)
+
+
